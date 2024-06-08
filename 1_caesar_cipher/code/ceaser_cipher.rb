28977dec9e4e40a4dcffb371c63be36ad34d57db
ayoung90 <ayoung90@live.com.au>
2024-06-08 21:42:00 +1000

    enhance input. allow for shift to be set. validate input

--- 1_caesar_cipher/code/ceaser_cipher.rb
+++ 1_caesar_cipher/code/ceaser_cipher.rb
@@ -1,16 +1,38 @@
 $cipher = Array('a'..'z')
 $shift = 5
 
+def init_shift_amount(number)
+  default = $shift
+
+  if number.integer? &&
+     (number >= 1 && number <= 5)
+    $shift = number
+  else
+    puts "Warning: number not entered. Has been kept at #{default}"
+  end
+end
+
 def encode_cipher(input)
   encoded_array = input.split('').map do |char|
-    $cipher[$cipher.index(char) + $shift]
+    if $cipher.include?(char)
+      $cipher[$cipher.index(char) + $shift]
+    else
+      char
+    end
   end
   encoded_array.join
 end
 
+puts '#### Welcome to Ceaser Cipher (use !exit to quit) ####'
+print 'Enter shift: '
+init_shift_amount(gets.chomp.to_i)
+
+input_message = 'Please enter text for encoding: '
+print input_message
+
 while (user_input = gets.chomp.downcase)
-  break if user_input == 'exit'
+  break if user_input == '!exit'
 
   puts encode_cipher(user_input)
-
+  print input_message
 end
