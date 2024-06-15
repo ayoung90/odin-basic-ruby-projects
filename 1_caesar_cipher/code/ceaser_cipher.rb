368085a93e3dba310b29376c42339e1f9b957663
ayoung90 <ayoung90@live.com.au>
2024-06-15 18:45:14 +1000

    Segregate ceasar cipher into method

--- 1_caesar_cipher/code/ceaser_cipher.rb
+++ 1_caesar_cipher/code/ceaser_cipher.rb
@@ -1,41 +1,21 @@
-$cipher = Array('a'..'z')
-$shift = 5
-
-def init_shift_amount(number)
-  default = $shift
-
-  if number.integer? &&
-     (number >= 1 && number <= 5)
-    $shift = number
-  else
-    puts "Warning: number not entered. Has been kept at #{default}"
+def ceaser_cipher(string, shift_factor)
+  all_letters = Array('a'..'z')
+  unless shift_factor.integer?
+    puts 'Warning: number not entered.'
+    return
   end
-end
 
-def encode_cipher(input)
-  encoded_array = input.split('').map do |char|
-    if $cipher.include?(char)
-      $cipher[$cipher.index(char) + $shift]
+  encoded_array = string.split('').map do |char|
+    if all_letters.include?(char)
+
+      index = all_letters.index(char) + shift_factor
+      index -= all_letters.length while index > all_letters.length
+      all_letters[index]
     else
       char
     end
   end
-  encoded_array.join
+  puts encoded_array.join
 end
 
-# TODO: u. Find out how to do this properly
-return if $test_mode
-
-puts '#### Welcome to Ceaser Cipher (use !exit to quit) ####'
-print 'Enter shift: '
-init_shift_amount(gets.chomp.to_i)
-
-input_message = 'Please enter text for encoding: '
-print input_message
-
-while (user_input = gets.chomp.downcase)
-  break if user_input == '!exit'
-
-  puts encode_cipher(user_input)
-  print input_message
-end
+ceaser_cipher('hello w%rld', 150)
