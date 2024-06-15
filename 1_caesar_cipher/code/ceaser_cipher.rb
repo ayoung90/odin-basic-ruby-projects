f3cf450c1b107384add2bf31c4d585fe38911a13
ayoung90 <ayoung90@live.com.au>
2024-06-15 18:52:52 +1000

    get prompt version working

--- 1_caesar_cipher/code/ceaser_cipher.rb
+++ 1_caesar_cipher/code/ceaser_cipher.rb
@@ -7,7 +7,6 @@ def ceaser_cipher(string, shift_factor)
 
   encoded_array = string.split('').map do |char|
     if all_letters.include?(char)
-
       index = all_letters.index(char) + shift_factor
       index -= all_letters.length while index > all_letters.length
       all_letters[index]
@@ -17,5 +16,3 @@ def ceaser_cipher(string, shift_factor)
   end
   puts encoded_array.join
 end
-
-ceaser_cipher('hello w%rld', 150)
---
+++ 1_caesar_cipher/code/ceaser_cipher_prompt.rb
@@ -0,0 +1,15 @@
+require './ceaser_cipher'
+
+puts '#### Welcome to Ceaser Cipher (use !exit to quit) ####'
+print 'Enter shift: '
+shift_amount = gets.chomp.to_i
+
+input_message = 'Please enter text for encoding: '
+print input_message
+
+while (user_input = gets.chomp.downcase)
+  break if user_input == '!exit'
+
+  puts ceaser_cipher(user_input, shift_amount)
+  print input_message
+end
