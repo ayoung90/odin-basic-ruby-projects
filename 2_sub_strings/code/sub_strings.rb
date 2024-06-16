273934bbe712712931ca492091e3e1a6c883d565
ayoung90 <ayoung90@live.com.au>
2024-06-16 14:39:04 +1000

    complete assignment and add test cases

--- 2_sub_strings/code/sub_strings.rb
+++ 2_sub_strings/code/sub_strings.rb
@@ -1,9 +1,15 @@
 def substrings(word, dictionary)
+  # lowercase both as word and dictionary can have mixed cases
+  word.downcase!
+  dictionary.map!(&:downcase)
+
   dictionary.each_with_object(Hash.new(0)) { |item, counts| counts[item] += 1 if word.include?(item) }
 end
 
 dictionary = %w[below down go going horn how howdy it i low own part partner sit]
 
+# should return a hash listing each substring (case insensitive) that was found in the original string and how many times it was found
 puts substrings('below', dictionary)
 
+# Next, make sure your method can handle multiple words
 puts substrings("Howdy partner, sit down! How's it going?", dictionary)
--- 2_sub_strings/spec/sub_strings_spec.rb
+++ 2_sub_strings/spec/sub_strings_spec.rb
@@ -1,2 +1,23 @@
-require 'spec_helper'
+require './spec/spec_helper'
 require_relative '../code/sub_strings'
+
+RSpec.describe 'Sub Strings' do
+  dictionary = %w[below down go going horn how howdy it i low own part partner sit]
+
+  describe 'Check single word' do
+    it 'Single word should have 2 items in dictionary' do
+      expected_hash = { 'below' => 1, 'low' => 1 }
+
+      expect(substrings('below', dictionary)).to eq(expected_hash)
+    end
+  end
+
+  describe 'Check multiple words' do
+    it 'Multiple word should have 11 items in dictionary' do
+      expected_hash = { 'down' => 1, 'go' => 1, 'going' => 1, 'how' => 1, 'howdy' => 1, 'it' => 1, 'i' => 1, 'own' => 1, 'part' => 1,
+                        'partner' => 1, 'sit' => 1 }
+
+      expect(substrings("Howdy partner, sit down! How's it going?", dictionary)).to eq(expected_hash)
+    end
+  end
+end
